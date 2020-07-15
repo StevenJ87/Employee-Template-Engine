a36748eaf22c4c7ff257776177469f32c2f3bf4f
Steven Johnson <sjohnson21960@gmail.com>
2020-07-14 22:21:11 -0500

    Manager test passed

--- Develop/lib/Engineer.js
+++ Develop/lib/Engineer.js
@@ -5,6 +5,11 @@ class Engineer extends Employee {
     constructor(name,id,email,github){
         super(name,id,email);
         this.github = github;
+        this.role = "Engineer";
+    };
+
+    getGithub(){
+        return this.github;
     };
 }
 
